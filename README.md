05adff5143519874d9a02c71d8a6d1832a429b16
vxsparsh <meowmeow@xoxo.xo>
2025-05-20 19:51:36 +0530

    fix

---
+++ README.md
@@ -0,0 +1,19 @@
+# This is my tmux config:
+
+
+### Features: 
+1. Show local (eth0) IP address in status bar.
+2. Supports mouse functions. i.e. Mouse clicks can be used to switch splits, windows, sessions etc. Scrolling with mouse wheel activates copy mode and you can select using mouse cursor.
+
+```
+git clone https://github.com/vxsparsh/tmux_config
+cd tmux_config
+./install.sh
+rm -rf icons-in-terminal
+```
+```
+    |\__/,|   (`\
+  _.|o o  |_   ) )
+-(((---(((--------
+meow
+```
---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+requirements="[+] Downloading requirements..."
+
+for ((i=0; i<${#requirements}; i++)); do
+    echo -n "${requirements:$i:1}"
+    sleep 0.05
+done
+echo
+
+sudo apt install tmux xclip dconf-cli
+
+setup="[+] Setting up tmux config..."
+for ((i=0; i<${#setup}; i++)); do
+    echo -n "${setup:$i:1}"
+    sleep 0.05
+done
+echo
+
+cp snipesec_tmux.conf ~/.tmux.conf
+mkdir -p ~/Scripts
+cp showIP_VM.sh ~/Scripts/showIP_VM.sh
+chmod +x ~/Scripts/showIP_VM.sh
+
+tmux_plugin="adding tmux plugin manager..."
+for ((i=0; i<${#tmux_plugin}; i++)); do
+    echo -n "${tmux_plugin:$i:1}"
+    sleep 0.05
+done
+echo
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Press Prefix+I inside tmux to install the plugins"
+
+icons_install="[+] Installing icons in terminal..."
+for ((i=0; i<${#icons_install}; i++)); do
+    echo -n "${icons_install:$i:1}"
+    sleep 0.05
+done
+echo
+
+cd ~/ ; git clone https://github.com/sebastiencs/icons-in-terminal.git
+cd icons-in-terminal;chmod +x install.sh
+./install.sh
+
+setup_done="[-] D0ne!"
+for ((i=0; i<${#setup_done}; i++)); do
+    echo -n "${setup_done:$i:1}"
+    sleep 0.05
+done
+echo
+
+exit_txt="Exiting Script Now..."
+for ((i=0; i<${#exit_txt}; i++)); do
+    echo -n "${exit_txt:$i:1}"
+    sleep 0.05
+done
+echo
+
+echo "open new terminal for the change."
